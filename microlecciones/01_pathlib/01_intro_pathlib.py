65ee8ea5f035c9922841c73ed30c3725657252d4
hans-rafael <hansvekoni@gmail.com>
2026-03-31 02:36:43 -0300

    Tarea 1 completada

--- microlecciones/01_pathlib/01_intro_pathlib.py
+++ microlecciones/01_pathlib/01_intro_pathlib.py
@@ -24,8 +24,8 @@ print("[Paso 1] Tomando la ruta actual del proceso...")
 ruta_actual = Path(".").resolve()
 
 print("[Paso 2] Consultando propiedades de la ruta...")
-existe = ruta_actual.exists()
-es_directorio = ruta_actual.is_dir()
+existe = ruta_actual.exists() # devuelve un boleano
+es_directorio = ruta_actual.is_dir() # devuelve un boleano si existe el
 
 print("[Resultado] Ruta absoluta:", ruta_actual)
 print("[Resultado] Existe:", existe)
@@ -33,3 +33,14 @@ print("[Resultado] Es directorio:", es_directorio)
 
 print("[Ejercicio] Cambia Path('.') por Path('docs') y compara los resultados de existe/es_directorio.")
 print("[Pregunta] ¿Que informacion te da resolve() sobre la ruta actual?")
+## resolve() da la ruta completa (absoluta) desde la "raíz" de el disco duro.
+## y con "." me da la ruta a donde estoy parado
+
+print(f"\n[Ahora] Cambia Path('.') por Path('docs') ")
+ruta_actual = Path("docs").resolve()
+print(f"[Resultado] ahora muestra tanto la ruta absoluta como,\n {ruta_actual} ")
+existe = ruta_actual.exists() 
+es_directorio = ruta_actual.is_dir()
+print("[Resultado] Existe:", existe)
+print("[Resultado] Es directorio:", es_directorio)
+print(f"\n le agrega /docs al final de la ruta pero me dice que no existe ahora ni ruta_actual ni es directorio")
